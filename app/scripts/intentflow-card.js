8d7a51ecca40ea89757b94cd5716dddfddcd93ce
rmpraba <rmpraba@gmail.com>
2016-05-06 14:38:52 +0530

    appln link n code removed

--- app/scripts/intentflow-card.js
+++ app/scripts/intentflow-card.js
@@ -26,37 +26,10 @@
         this.querySelector("#" + e.target.id).style.backgroundColor = '#bfbfbf';
         this.flag = e.target.id;
       }
-      //alert(e.target.id);
-      /*alert(this.flag)
-      if(this.flag!="true"){
-       this.querySelector("#" + this.idval[1] + "1").id = this.idval[1];
-       this.querySelector("#" + this.idval[1]).style.backgroundColor = '#f2f2f2'; 
-      }
-      for(var i=0;i<this.idval.length;i++)
-      {
-       if(e.target.id==this.idval[i]){       
-       this.querySelector("#" + this.idval[i] + "1").id = this.idval[i];
-       this.querySelector("#" + this.idval[i]).style.backgroundColor = '#bfbfbf';
-       this.flag = this.idval[i];   
-       }
-      }*/
+     
       localStorage.setItem('curr_sess_postate',e.target.id);
       this.$.grnservice.FnIntentitemReadService();
      }
-     /*,
-     setStateCreate:function(){     
-     if (localStorage.getItem("curr_sess_roleflag")=="4") { 
-       this.querySelector("#" + this.idval[0] + "1").id = this.idval[0];
-       this.querySelector("#" + this.idval[0]).style.backgroundColor = '#bfbfbf';        
-       this.flag = this.idval[0];      
-     }
-     },
-     setStateTransition:function(){     
-     
-       this.querySelector("#" + this.idval[1] + "1").id = this.idval[1];
-       this.querySelector("#" + this.idval[1]).style.backgroundColor = '#bfbfbf';        
-       this.flag = this.idval[1];      
-     
-     }*/
+    
     });
   })();
