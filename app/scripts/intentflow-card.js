817fe73f74fd3b9d166d782f6518ab6a0778f996
rmpraba <rmpraba@gmail.com>
2016-05-06 13:29:39 +0530

    inflow state

--- app/scripts/intentflow-card.js
+++ app/scripts/intentflow-card.js
@@ -7,14 +7,39 @@
       localStorage.setItem('curr_sess_postate','Approved');
       this.flag='true';
       this.idval = ["Created", "Approved", "POCreated", "POSent"];
-      if (localStorage.getItem("curr_sess_roleflag")=="4") { 
-       this.querySelector("#" + this.idval[0] + "1").id = this.idval[0];
-       this.querySelector("#" + this.idval[0]).style.backgroundColor = '#bfbfbf';        
-       this.flag = this.idval[0];      
-      }
+      // if (localStorage.getItem("curr_sess_roleflag")=="4") { 
+       this.querySelector("#" + this.idval[1] + "1").id = this.idval[1];
+       this.querySelector("#" + this.idval[1]).style.backgroundColor = '#bfbfbf';        
+       this.flag = this.idval[1];      
+      // }
      },
      click:function(e){
+      if (this.flag == "true") {
+        this.querySelector("#" + e.target.id + "1").id = e.target.id;
+        this.querySelector("#" + e.target.id).style.backgroundColor = '#bfbfbf';
+        this.flag = e.target.id;
+      }
+      else {
+        this.querySelector("#" + this.flag).style.backgroundColor = '#f2f2f2';
+        this.querySelector("#" + this.flag).id = this.flag + "1";
+        this.querySelector("#" + e.target.id + "1").id = e.target.id;
+        this.querySelector("#" + e.target.id).style.backgroundColor = '#bfbfbf';
+        this.flag = e.target.id;
+      }
       //alert(e.target.id);
+      /*alert(this.flag)
+      if(this.flag!="true"){
+       this.querySelector("#" + this.idval[1] + "1").id = this.idval[1];
+       this.querySelector("#" + this.idval[1]).style.backgroundColor = '#f2f2f2'; 
+      }
+      for(var i=0;i<this.idval.length;i++)
+      {
+       if(e.target.id==this.idval[i]){       
+       this.querySelector("#" + this.idval[i] + "1").id = this.idval[i];
+       this.querySelector("#" + this.idval[i]).style.backgroundColor = '#bfbfbf';
+       this.flag = this.idval[i];   
+       }
+      }*/
       localStorage.setItem('curr_sess_postate',e.target.id);
       this.$.grnservice.FnIntentitemReadService();
      }
